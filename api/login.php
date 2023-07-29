445a6726ee98196718dfa979e7dc31b11e909a85
shivpratap49 <spsinghrajawat49@gmail.com>
2023-07-29 08:17:21 +0530

    read api make

--- api/login.php
+++ api/login.php
@@ -11,6 +11,7 @@ $table=$data['table'];
 $user=$data['username'];
 $pass=md5($data['password']);
 
+
 $sql="SELECT*FROM login WHERE employee_code='$user' AND password='$pass'" ;
 $result=pg_query($conn,$sql);
 $row = pg_fetch_array($result);
@@ -18,18 +19,21 @@ if(pg_num_rows($result)>0){
     $sql1="SELECT employee_code FROM $table WHERE employee_code='$user' " ;
     $result1=pg_query($conn,$sql1);
     if(pg_num_rows($result1)>0){
-  session_start();
-  $_SESSION['loggedin']=true; 
-  $_SESSION['employee_code']=$user;
-  $_SESSION['table']=$table;
-  $_SESSION['dob']=$pass;
-  echo json_encode(array('Message'=>'Record Inserted Successfully','Status'=>true));
+ 	$token=md5((rand(1000000,10000000000000)));
+ 	$sql2="UPDATE login set token='$token' where employee_code='$user'";
+ 	if(pg_query($conn,$sql2)){
+ 	  echo json_encode(array('Message'=>'Record Inserted Successfully','Status'=>true,'table'=>"$table",'username'=>"$user",'token'=>"$token"));
+ 	}else{
+ 	echo json_encode(array('Message'=>'Token not generated','Status'=>false,));
+ 	}
+  
     }else{
-  echo json_encode(array('Message'=>'Eroor','Status'=>false));
+  echo json_encode(array('Message'=>'Please choose user catogery','Status'=>false));
 }
+
 }  
 else{
- $log=false;
+ echo json_encode(array('Message'=>'Invalid user','Status'=>false));
 }
 
 
--- api/read.php
+++ api/read.php
@@ -1,15 +1,32 @@
 <?php
 header('Content-Type:application/json');
 header('Access-Control-Allow-Origin:*');
-//$data=json_decode(file_get_contents("php://input"),true);
+
+$data=json_decode(file_get_contents("php://input"),true);
+$table=$data['table'];
+$employee_code=$data['username'];
+$token=$data['token'];
 include "config.php";
-$sql = "SELECT*FROM person";
+if($token){
+$sql = "SELECT*FROM login WHERE token='$token'";
 $result = pg_query($conn, $sql);
-//$row = pg_fetch_array($result);
-if (pg_num_rows($result) >0){
-    $output=pg_fetch_all($result,PGSQL_ASSOC);
+if(pg_num_rows($result) >0){
+$sql1 = "SELECT*FROM $table WHERE employee_code='$employee_code'";
+$result1 = pg_query($conn, $sql1);
+//$row = pg_fetch_array($result1);
+if (pg_num_rows($result1) >0){
+    $output=pg_fetch_all($result1,PGSQL_ASSOC);
     print_r (json_encode($output)) ;
 }else{
     echo json_encode(array('Message'=>'no record founf','Status'=>false));
 }
+}else
+{
+echo json_encode(array('Message'=>'invalid token','Status'=>false));
+}
+}
+else{
+echo json_encode(array('Message'=>'invalid token','Status'=>false));
+}
+
 ?>
